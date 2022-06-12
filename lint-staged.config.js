6be12bf6b5ce1708577a804a1a8769fbb79f5e7b
Eric Opoku <20393990+ericus20@users.noreply.github.com>
2022-06-12 19:51:41 -0400

    build(upsidle): configure husky to install on postinstall (#9)
    * build(upsidle): configure husky to install on postinstall
    
    * ci(upsidle): restrict commit directly to develop or master branch

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,19 +1,16 @@
 module.exports = {
   // this will check Typescript files in customer portal
   "**/customer-portal/*.(ts|tsx)": () =>
-    "yarn --cwd customer-portal tsc --noEmit",
+      "yarn --cwd customer-portal tsc --noEmit",
 
   // this will check Typescript files in admin portal
   "**/admin-portal/*.(ts|tsx)": () => "yarn --cwd admin-portal tsc --noEmit",
 
-  // this will check lint on all matched files for admin and customer portal
+  // this will check lint and formatting on all matched files for admin and customer portal
   "**/*-portal/*.(js|json|md|mdx|ts|tsx|yaml|yml)": () =>
-    "yarn --cwd customer-portal lint:ci",
-
-  // this will check formatting on all matched files for admin and customer portal
-  "**/*-portal/*.(js|json|md|mdx|ts|tsx|yaml|yml)": () =>
-    "yarn --cwd customer-portal format-check",
+      ["yarn --cwd customer-portal lint:ci",
+        "yarn --cwd customer-portal format-check"],
 
   // this will check formatting for java and gradle files in api
-  "**/api/*.(java|gradle)": () => "yarn api-format-check",
+  "**/api/*.(java|gradle)": () => "yarn format-check-api",
 };
