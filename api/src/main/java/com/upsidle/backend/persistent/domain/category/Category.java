4c6befa8c1c896f834d85529e927ebe473fa22ef
stevartz <58922482+stevartz@users.noreply.github.com>
2022-06-13 23:09:00 -0400

    Created category class with the needed tests (#8)
    * Created category class with the needed tests
    
    * Fixed issues with spotlessapply on category entity
    
    * Removed @table annotation and updated the comments
    
    * feat(api): remove @table annotation and update comment

---
+++ api/src/main/java/com/upsidle/backend/persistent/domain/category/Category.java
@@ -0,0 +1,59 @@
+package com.upsidle.backend.persistent.domain.category;
+
+import com.upsidle.backend.persistent.domain.base.BaseEntity;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * The category model for the application.
+ *
+ * @author Stephen Boakye
+ * @version 1.0
+ * @since 1.0
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Category extends BaseEntity<Long> implements Serializable {
+
+  @Serial private static final long serialVersionUID = 2412831733001851062L;
+
+  @Column(unique = true, nullable = false)
+  @NotBlank(message = "Category name is required")
+  private String name;
+
+  public Category(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Category category) || !super.equals(o)) {
+      return false;
+    }
+    return Objects.equals(getName(), category.getName());
+  }
+
+  @Override
+  protected boolean canEqual(Object other) {
+    return other instanceof Category;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), getName());
+  }
+}
