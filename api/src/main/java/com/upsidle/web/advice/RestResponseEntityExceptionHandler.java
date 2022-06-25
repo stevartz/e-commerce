7fdfc953ba4775ec1d9644950c286e559996e6b4
Matt <mpuent18@gmail.com>
2022-06-25 12:18:51 -0400

    feat(upl): cherry picked from user registration branch

--- api/src/main/java/com/upsidle/web/advice/RestResponseEntityExceptionHandler.java
+++ api/src/main/java/com/upsidle/web/advice/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.upsidle.web.advice;
 
+import com.upsidle.exception.user.UserAlreadyExistsException;
+import com.upsidle.exception.user.UserDtoNullOnCreationException;
+import com.upsidle.web.payload.pojo.ApiError;
+import java.util.ArrayList;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,5 +35,52 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
   protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
     String message = ex.getMessage();
     return handleExceptionInternal(ex, message, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
+
+  /**
+   * Handles UserAlreadyExistsException thrown by the REST API.
+   *
+   * @param ex The exception thrown by the REST API.
+   * @param request The request object.
+   * @return A ResponseEntity object.
+   */
+  @ExceptionHandler(value = {UserAlreadyExistsException.class, UserDtoNullOnCreationException.class})
+  protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
+    String message = ex.getMessage();
+    var error = new ApiError(HttpStatus.BAD_REQUEST, message);
+    return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+  }
+
+  /**
+   * Handles MethodArgumentNotValidException thrown by the REST API by @Valid annotation.
+   *
+   * @param ex the exception
+   * @param headers the headers to be written to the response
+   * @param status the selected response status
+   * @param request the current request
+   * @return the response entity
+   */
+  @NonNull
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      @NonNull MethodArgumentNotValidException ex,
+      @NonNull HttpHeaders headers,
+      @NonNull HttpStatus status,
+      @NonNull WebRequest request) {
+
+    var errors = new ArrayList<String>();
+    // Get all the fields that failed in the @valid then add them to the errors array
+    for (var error : ex.getBindingResult().getFieldErrors()) {
+      errors.add(String.join(": ", error.getField(), error.getDefaultMessage()));
+    }
+
+    // Get all global errors as well to be added to the errors list.
+    for (var error : ex.getBindingResult().getGlobalErrors()) {
+      errors.add(String.join(": ", error.getObjectName(), error.getDefaultMessage()));
+    }
+
+    // Create an instance of the new ApiError with the details of the error.
+    var error = new ApiError(status, "Method argument not valid", errors);
+    return handleExceptionInternal(ex, error, headers, status, request);
   }
 }
--- api/src/main/java/com/upsidle/web/rest/v1/UserRestApi.java
+++ api/src/main/java/com/upsidle/web/rest/v1/UserRestApi.java
@@ -1,17 +1,36 @@
 package com.upsidle.web.rest.v1;
 
+import com.upsidle.annotation.Loggable;
+import com.upsidle.backend.persistent.domain.user.User;
+import com.upsidle.backend.service.mail.EmailService;
+import com.upsidle.backend.service.security.EncryptionService;
+import com.upsidle.backend.service.security.JwtService;
 import com.upsidle.backend.service.user.UserService;
 import com.upsidle.constant.AdminConstants;
+import com.upsidle.constant.user.UserConstants;
 import com.upsidle.enums.OperationStatus;
+import com.upsidle.enums.RoleType;
+import com.upsidle.exception.user.UserAlreadyExistsException;
+import com.upsidle.exception.user.UserDtoNullOnCreationException;
+import com.upsidle.shared.dto.UserDto;
+import com.upsidle.shared.util.UserUtils;
+import com.upsidle.web.payload.request.SignUpRequest;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +45,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(AdminConstants.API_V1_USERS_ROOT_URL)
-@PreAuthorize("isFullyAuthenticated() && hasRole(T(com.upsidle.enums.RoleType).ROLE_ADMIN)")
 public class UserRestApi {
 
   private final UserService userService;
+
+  private final JwtService jwtService;
+
+  private final EncryptionService encryptionService;
+
+  private final EmailService emailService;
 
   /**
    * Enables the user associated with the publicId.
@@ -70,5 +94,49 @@ public class UserRestApi {
     userService.deleteUser(publicId);
 
     return ResponseEntity.ok(OperationStatus.SUCCESS);
+  }
+
+  @Loggable
+  @PostMapping
+  @SecurityRequirements
+  public ResponseEntity<OperationStatus> register(@RequestBody @Valid SignUpRequest signUpRequest) {
+
+    // convert sign to userDto
+    UserDto userDto = UserUtils.convertToUserDto(signUpRequest);
+
+    // check user exist by username or email.
+    if(userService.existsByUsernameOrEmailAndEnabled(userDto.getUsername(), userDto.getEmail())){
+      LOG.warn(UserConstants.USERNAME_OR_EMAIL_EXITS);
+      throw new UserAlreadyExistsException(UserConstants.USERNAME_OR_EMAIL_EXITS);
+    }
+
+    // Generate JWT token for the user using the username
+    String token = jwtService.generateJwtToken(userDto.getUsername());
+    // assign the new jwt token to the user as the userVerificationToken
+
+    userDto.setVerificationToken(token);
+    // now create the user using the user service and pass the user, user.getRoles()
+    RoleType roleType = RoleType.ROLE_ADMIN.toString().equals(userDto.getRole()) ? RoleType.ROLE_ADMIN : RoleType.ROLE_USER;
+    UserDto createdUser = userService.createUser(userDto, Collections.singleton(roleType));
+
+    // check if the returned user is null, then you throw exception
+    if(Objects.isNull(createdUser)) {
+      LOG.warn(UserConstants.USER_DTO_MUST_NOT_BE_NULL);
+      throw new UserDtoNullOnCreationException(UserConstants.USER_DTO_MUST_NOT_BE_NULL);
+    }
+    // otherwise, encrypt the jwt token (there is an EncryptionService)
+    // encode the encrypted token (we do not want to send the raw jwt token out)
+    String encryptedToken = encryptionService.encrypt(token);
+
+    String encodedToken = encryptionService.encode(encryptedToken);
+
+    // send email (sendAccountVerificationToken) using the user and encoded token)
+    emailService.sendAccountVerificationEmail(userDto, encodedToken);
+
+    // look into how to create a location from the current request and expand the id of the user
+
+    // update the return below to use the location instead of null.
+
+    return ResponseEntity.created(URI.create("/api/users/" + createdUser.getPublicId())).body(OperationStatus.SUCCESS);
   }
 }
