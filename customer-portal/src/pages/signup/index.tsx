156a9fc741ccbc209de070a2eef0c89c1519b9a4
George Anguah <Georgeanguah@yahoo.com>
2022-11-10 21:40:49 -0500

    feat(customer-portal): add basic validation to input signup page (#20)

--- customer-portal/src/pages/signup/index.tsx
+++ customer-portal/src/pages/signup/index.tsx
@@ -56,69 +56,91 @@ const SignUp: NextPage = () => {
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-
-            <label
-              htmlFor="username"
-              className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1  pt-1.5"
-            >
-              {labels[0]}
-            </label>
-            <input
-              id="username"
-              type="text"
-              className="b border border-grey-light w-full p-3 rounded mb-4"
-              name="username"
-              placeholder="Username"
-              onChange={onChangeUsername}
-              value={username}
-            />
-
-            <label
-              htmlFor="email"
-              className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
-            >
-              {labels[1]}
-            </label>
-            <input
-              id="email"
-              type="text"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="email"
-              placeholder="Email"
-              onChange={onChangeEmail}
-              value={email}
-            />
-            <label
-              htmlFor="password"
-              className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
-            >
-              {labels[2]}
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="password"
-              placeholder="Password"
-              onChange={onChangePassword}
-              value={password}
-            />
-            <label
-              htmlFor="confirm-password"
-              className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
-            >
-              {labels[3]}
-            </label>
-            <input
-              id="confirm-password"
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="confirm_password"
-              placeholder="Confirm Password"
-              onChange={onChangeConfirmPassword}
-              value={confirmPassword}
-            />
-
+            {/* Username  */}
+            <div className="flex flex-col">
+              <label
+                htmlFor="username"
+                className=" peer block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1  pt-1.5"
+              >
+                {labels[0]}
+              </label>
+              <input
+                id="username"
+                type="text"
+                required
+                className="peer first-letter:block border border-grey-light w-full p-3 rounded"
+                name="username"
+                placeholder="Username"
+                onChange={onChangeUsername}
+                // value={username}
+              />
+              <p className="invisible peer-invalid:visible  text-xs text-red-700 mb-1 mt-0">
+                Enter your username
+              </p>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
+              >
+                {labels[1]}
+              </label>
+              <input
+                id="email"
+                type="email"
+                required
+                className="peer selection:block border border-grey-light w-full p-3 rounded"
+                name="email"
+                placeholder="Email"
+                onChange={onChangeEmail}
+                value={email}
+              />
+              <p className="invisible peer-invalid:visible  text-xs text-red-700  mb-1 mt-0">
+                Enter your email
+              </p>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
+              >
+                {labels[2]}
+              </label>
+              <input
+                id="password"
+                type="password"
+                required
+                className="peer block border border-grey-light w-full p-3 rounded"
+                name="password"
+                placeholder="Password"
+                onChange={onChangePassword}
+                value={password}
+              />
+              <p className="invisible peer-invalid:visible  text-xs text-red-700  mb-1 mt-0">
+                Enter your password
+              </p>
+            </div>
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className=" block font-bold text-s w-full placeholder-gray-gray4 px-2 pb-1 pt-1.5"
+              >
+                {labels[3]}
+              </label>
+              <input
+                id="confirm-password"
+                type="password"
+                className="peer block border border-grey-light w-full p-3 rounded"
+                name="confirm_password"
+                placeholder="Confirm Password"
+                onChange={onChangeConfirmPassword}
+                value={confirmPassword}
+                required
+              />
+              <p className="invisible peer-invalid:visible  text-xs text-red-700  mb-4 mt-0">
+                Re-enter your password
+              </p>
+            </div>
             <button
               type="submit"
               className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1"
